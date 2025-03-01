77cb740895c05d5db75cc58ce8dd786826409964
Real-Aya <realaya@realaya.xyz>
2025-03-01 11:46:13 +0700

    Create 3183403065.lua

---
+++ serenity/game/3183403065.lua
@@ -0,0 +1,6 @@
+-- Anime Adventure
+if _G.SelectPlayMode ~= nil then
+    loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/Real-Aya/Crack/refs/heads/main/serenity/game/aa/full.lua'))();
+else
+    loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/Real-Aya/Crack/refs/heads/main/serenity/game/aa/nm.lua'))();
+end
